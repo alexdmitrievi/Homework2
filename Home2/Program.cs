a7dcbb05c08007499e94b4e7c1dd5e5d67ce8196
alexdmitrievi <103923985+alexdmitrievi@users.noreply.github.com>
2022-05-13 18:39:32 +0600

    Home2

---
+++ Home2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine()); 
+if (a == b*b) {
+    Console.Write("да");
+}
+else {
+    Console.Write("нет");
+}
+
