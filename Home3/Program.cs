372db1db707f9a10e4a3c9cfab860a5e93c6155d
alexdmitrievi <103923985+alexdmitrievi@users.noreply.github.com>
2022-05-13 19:02:12 +0600

    Home3

---
+++ Home3/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("введите номер четверти");
+int a = Convert.ToInt32(Console.ReadLine()); 
+if (a==1) {
+    Console.WriteLine("x - положительное число");
+    Console.WriteLine("y - положительное число");
+}
+else if (a==2){
+    Console.WriteLine("x - отрицательное число");
+    Console.WriteLine("y - положительное число");
+}
+else if (a==3){
+    Console.WriteLine("x - отрицательное число");
+    Console.WriteLine("y - отрицательное число");
+}
+else {
+    Console.WriteLine("x - положительное число");
+    Console.WriteLine("y - отрицательное число");
+}
