35067192886a9b4b9839479858aa9ac79c771c8b
alexdmitrievi <103923985+alexdmitrievi@users.noreply.github.com>
2022-05-16 19:47:05 +0600

    4,5

---
+++ Home5/Program.cs
@@ -0,0 +1,23 @@
+﻿Double x1=0;
+Double x2=0;
+Double y1=0;
+Double y2=0;
+Double z1=0;
+Double z2=0;
+
+Console.WriteLine("введите координату x первой точки");
+x1 = Convert.ToDouble(Console.ReadLine()); 
+Console.WriteLine("введите координату y первой точки");
+y1 = Convert.ToDouble(Console.ReadLine()); 
+Console.WriteLine("введите координату z первой точки");
+z1 = Convert.ToDouble(Console.ReadLine()); 
+
+Console.WriteLine("введите координату x второй точки");
+x2 = Convert.ToDouble(Console.ReadLine()); 
+Console.WriteLine("введите координату y второй точки");
+y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("введите координату z второй точки");
+z2 = Convert.ToDouble(Console.ReadLine()); 
+
+double res = Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)+(z1-z2)*(z1-z2));
+Console.WriteLine(res);
