c45432f7a66b8e8ad466f37705326aef61b74d39
alexdmitrievi <103923985+alexdmitrievi@users.noreply.github.com>
2022-05-16 20:22:34 +0600

    pol

---
+++ Home6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("введите пятизначное число");
+int a = Convert.ToInt32(Console.ReadLine()); 
+int[] digits = new int[3];
+digits[0] = a%10;
+digits[1] = a/10%10;
+digits[2] = a/100%10;
+int result = digits[0]*10000+digits[1]*1000+digits[2]*100+digits[1]*10+digits[0]*1;
+if (a == result) {
+    Console.WriteLine("да");
+}
+else {
+    Console.WriteLine("нет");
+}
