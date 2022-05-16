5cf55dabba7aa455f3495cd27a144382817459ae
alexdmitrievi <103923985+alexdmitrievi@users.noreply.github.com>
2022-05-16 19:49:08 +0600

    homework2

---
+++ Home4/Program.cs
@@ -0,0 +1,14 @@
+﻿Double x1=0;
+Double x2=0;
+Double y1=0;
+Double y2=0;
+Console.WriteLine("введите координату x первой точки");
+x1 = Convert.ToDouble(Console.ReadLine()); 
+Console.WriteLine("введите координату y первой точки");
+y1 = Convert.ToDouble(Console.ReadLine()); 
+Console.WriteLine("введите координату x второй точки");
+x2 = Convert.ToDouble(Console.ReadLine()); 
+Console.WriteLine("введите координату y второй точки");
+y2 = Convert.ToDouble(Console.ReadLine());
+double res = Math.Sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+Console.WriteLine(res);
